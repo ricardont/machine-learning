be88e66b16a2cce60606c8e17113169434a7c075
ricardont <rontiverosmarquez@gmail.com>
2019-12-17 06:25:09 -0700

    python sample

---
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+

8d25eb70b83a31448a0524520a3c1a6944d5985d
ricardont <rontiverosmarquez@gmail.com>
2020-01-05 14:06:09 -0700

    missing data

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -13,7 +13,7 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 #Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN', strategy="mean", axis=0)
-imputer.fit(X[:, 1:3])
+from sklearn.impute import SimpleImputer
+imputer   = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+X[:, 1:3] = imputer.fit_transform(X[:,1:3])
 
--- Part 1 - Data Preprocessing/missing_data.R
+++ Part 1 - Data Preprocessing/missing_data.R
@@ -1,12 +1,10 @@
-# Data Preprocessing
-
+# Data Preprocessing Template
 # Importing the dataset
 dataset = read.csv('Data.csv')
+dataset$Age= ifelse(is.na(dataset$Salary),
+                    ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                    dataset$Age)
 
-# Taking care of missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                        dataset$Salary)
+dataset$Salary= ifelse(is.na(dataset$Salary),
+                    ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                    dataset$Salary)

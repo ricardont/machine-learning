a227b1c1f95a64fce030107dd5c9e11fc5bdf032
ricardont <rontiverosmarquez@gmail.com>
2020-01-08 06:26:35 -0700

    encoding

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -16,4 +16,13 @@ y = dataset.iloc[:, 3].values
 from sklearn.impute import SimpleImputer
 imputer   = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
 X[:, 1:3] = imputer.fit_transform(X[:,1:3])
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
 
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+onehotencoder = OneHotEncoder(categorical_features = [0])
